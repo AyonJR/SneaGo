d2b6cfbb6340cbe4504a02a7d68a5499a5a4798f
unknown <abdulbaset.ayon@gmail.com>
2024-09-23 14:42:17 +0600

    addded new section

--- src/Home/Grids.jsx
+++ src/Home/Grids.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Grids = () => {
   return (
     <div className="flex gap-3 w-full">
-      <div className="relative w-1/3 h-[400px]">
+      <div className="relative w-1/3 h-[300px]">
         <img
           className="object-cover h-full w-full"
           src="https://i.ibb.co/ZXJzPj7/jeff-tumale-SD9-Jyl1x-NQ4-unsplash.jpg"
@@ -12,8 +12,8 @@ const Grids = () => {
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-80" />
       </div>
 
-      <div className="w-1/3 grid grid-rows-2 h-[400px]">
-        <div className="relative h-[200px]">
+      <div className="w-1/3 grid grid-rows-2 h-[300px]">
+        <div className="relative h-[150px]">
           <img
             className="object-cover h-full w-full"
             src="https://i.ibb.co/SwHH8Rg/jordan-andrews-A5-EUGGHqs4-A-unsplash-1.jpg"
@@ -21,7 +21,7 @@ const Grids = () => {
           />
           <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-80" />
         </div>
-        <div className="relative h-[200px] overflow-hidden">
+        <div className="relative h-[150px] overflow-hidden">
           <img
             className="object-cover h-full w-full"
             src="https://i.ibb.co/0Yt1kn4/dominik-martin-b2-f-BVrfx0o-unsplash.jpg"
@@ -31,7 +31,7 @@ const Grids = () => {
         </div>
       </div>
 
-      <div className="relative w-1/3 h-[400px]">
+      <div className="relative w-1/3 h-[300px]">
         <img
           className="object-cover h-full w-full"
           src="https://i.ibb.co.com/mDbmhH6/amirhossein-hasani-3-MLjh-MAJm-Y-unsplash.jpg"
